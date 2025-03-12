a7c28b6536b2110dc696fc2ea96d12515c1e14cb
leomontenegro2104 <eng.leonardomontenegro@gmail.com>
2025-03-12 15:34:04 -0300

    chore: update tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
   server: { port: 3000 },
-  plugins: [react(),tsconfigPaths(),],
+  plugins: [tailwindcss(), react(), tsconfigPaths()],
   resolve: {
     alias: {
-      "@src": "/src",
-      "@components": "/src/components",
-      "@pages": "/src/pages",
-      "@routers": "/src/routers"
-    }
+      '@src': '/src',
+      '@components': '/src/components',
+      '@pages': '/src/pages',
+      '@routers': '/src/routers',
+    },
   },
   build: {
     sourcemap: true,
     outDir: 'build',
   },
-})
+});
