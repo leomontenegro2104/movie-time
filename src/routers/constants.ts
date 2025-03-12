e0ab66b9768239de2c4c9a51542cebb1586e2658
leomontenegro2104 <eng.leonardomontenegro@gmail.com>
2025-03-12 14:58:54 -0300

    feat: routes

---
+++ src/routers/constants.ts
@@ -0,0 +1,6 @@
+export const LOGIN_PATH = '/login';
+export const SIGNUP_PATH = '/signup';
+export const HOME_PATH = '/';
+export const CATALOG_PATH = '/:category';
+export const SEARCH_CATALOG_PATH = '/:category/search/:keyword';
+export const DETAIL_PATH = '/:category/:id';
