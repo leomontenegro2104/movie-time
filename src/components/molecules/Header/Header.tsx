d6c7177fca10cc62b7aac1de14870766883566d2
leomontenegro2104 <eng.leonardomontenegro@gmail.com>
2025-03-15 09:53:31 -0300

    refactor: header

--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -1,6 +1,5 @@
 import React, { useRef, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import './header.scss';
 import logo from '../../../assets/tmovie.png';
 
 interface NavItem {
@@ -9,29 +8,20 @@ interface NavItem {
 }
 
 const headerNav: NavItem[] = [
-  {
-    display: 'Home',
-    path: '/'
-  },
-  {
-    display: 'Movies',
-    path: '/movie'
-  },
-  {
-    display: 'TV Series',
-    path: '/tv'
-  }
+  { display: 'Home', path: '/' },
+  { display: 'Movies', path: '/movie' },
+  { display: 'TV Series', path: '/tv' },
 ];
 
 const Header: React.FC = () => {
   const { pathname } = useLocation();
   const headerRef = useRef<HTMLDivElement | null>(null);
 
-  const active = headerNav.findIndex(e => e.path === pathname);
+  const active = headerNav.findIndex((e) => e.path === pathname);
 
   useEffect(() => {
     const shrinkHeader = () => {
-      if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      if (window.scrollY > 100) {
         headerRef.current?.classList.add('shrink');
       } else {
         headerRef.current?.classList.remove('shrink');
@@ -39,28 +29,50 @@ const Header: React.FC = () => {
     };
 
     window.addEventListener('scroll', shrinkHeader);
-    return () => {
-      window.removeEventListener('scroll', shrinkHeader);
-    };
+    return () => window.removeEventListener('scroll', shrinkHeader);
   }, []);
 
   return (
-    <div ref={headerRef} className="header">
-      <div className="header__wrap container">
-        <div className="logo">
-          <img src={logo} alt="tMovies logo" />
-          <Link to="/">Movie time</Link>
+    <div
+      ref={headerRef}
+      className="fixed top-0 left-0 w-full h-20 z-50 transition-all duration-300 bg-transparent shrink:bg-black"
+    >
+      <div className="container flex items-center justify-between h-full px-8">
+        <div className="flex items-center space-x-3 text-2xl font-bold">
+          <img src={logo} alt="tMovies logo" className="w-12 md:w-8" />
+          <Link to="/" className="text-white">
+            Movie Time
+          </Link>
         </div>
-        <ul className="header__nav">
+        <ul className="hidden md:flex items-center space-x-8 text-lg font-semibold">
           {headerNav.map((e, i) => (
-            <li key={i} className={`${i === active ? 'active' : ''}`}>
-              <Link to={e.path}>
+            <li key={i} className="relative">
+              <Link
+                to={e.path}
+                className={`text-white pb-1 relative ${i === active ? 'after:w-full' : 'after:w-0'
+                  } after:absolute after:bottom-0 after:left-1/2 after:-translate-x-1/2 after:h-[2px] after:bg-red-500 after:transition-all after:duration-500 hover:after:w-full`}
+              >
                 {e.display}
               </Link>
             </li>
           ))}
         </ul>
       </div>
+
+      {/* Mobile navigation */}
+      <ul className="md:hidden fixed bottom-0 left-0 w-full h-16 bg-black flex items-center justify-around text-lg font-semibold shadow-md">
+        {headerNav.map((e, i) => (
+          <li key={i}>
+            <Link
+              to={e.path}
+              className={`text-white pb-1 relative ${i === active ? 'after:w-full' : 'after:w-0'
+                } after:absolute after:bottom-0 after:left-1/2 after:-translate-x-1/2 after:h-[2px] after:bg-red-500 after:transition-all after:duration-500 hover:after:w-full`}
+            >
+              {e.display}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
