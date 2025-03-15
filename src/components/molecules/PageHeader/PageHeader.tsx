40e6c16fd2face9d245c5163cf95256c13a74fee
leomontenegro2104 <eng.leonardomontenegro@gmail.com>
2025-03-15 09:54:20 -0300

    refactor: page header

--- src/components/molecules/PageHeader/PageHeader.tsx
+++ src/components/molecules/PageHeader/PageHeader.tsx
@@ -1,5 +1,4 @@
 import React, { ReactNode } from 'react';
-import './page-header.scss';
 import bg from '../../../assets/footer-bg.jpg';
 
 interface PageHeaderProps {
@@ -8,8 +7,12 @@ interface PageHeaderProps {
 
 const PageHeader: React.FC<PageHeaderProps> = ({ children }) => {
   return (
-    <div className="page-header" style={{ backgroundImage: `url(${bg})` }}>
-      <h2>{children}</h2>
+    <div
+      className="relative text-center mb-8 pt-[8rem] pb-[2rem] bg-top bg-cover bg-no-repeat"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      <h2 className="relative z-[99]">{children}</h2>
+      <div className="absolute bottom-0 left-0 w-full h-[100px] bg-gradient-to-t from-[#0f0f0f] to-transparent"></div>
     </div>
   );
 };
