d0b40348b4c4cd78c4e19d93b62b68039e2bc279
leomontenegro2104 <eng.leonardomontenegro@gmail.com>
2025-03-15 09:53:20 -0300

    refactor: footer

--- src/components/molecules/Footer/Footer.tsx
+++ src/components/molecules/Footer/Footer.tsx
@@ -1,35 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-import './footer.scss';
-
 import bg from '../../../assets/footer-bg.jpg';
 import logo from '../../../assets/tmovie.png';
 
 const Footer: React.FC = () => {
   return (
-    <div className="footer" style={{ backgroundImage: `url(${bg})` }}>
-      <div className="footer__content container">
-        <div className="footer__content__logo">
-          <div className="logo">
-            <img src={logo} alt="tMovies Logo" />
-            <Link to="/">tMovies</Link>
+    <div className="relative py-24 px-8 bg-top bg-cover" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="max-w-[1000px] mx-auto">
+        <div className="flex justify-center items-center mb-12">
+          <div className="flex items-center space-x-3">
+            <img src={logo} alt="tMovies Logo" className="w-10" />
+            <Link to="/" className="text-2xl font-bold text-white">tMovies</Link>
           </div>
         </div>
-        <div className="footer__content__menus">
-          <div className="footer__content__menu">
+        <div className="grid grid-cols-3 gap-6 md:grid-cols-2">
+          <div className="flex flex-col text-lg font-semibold space-y-4">
             <Link to="/">Home</Link>
             <Link to="/">Contact us</Link>
             <Link to="/">Term of services</Link>
             <Link to="/">About us</Link>
           </div>
-          <div className="footer__content__menu">
+          <div className="flex flex-col text-lg font-semibold space-y-4">
             <Link to="/">Live</Link>
             <Link to="/">FAQ</Link>
             <Link to="/">Premium</Link>
             <Link to="/">Privacy policy</Link>
           </div>
-          <div className="footer__content__menu">
+          <div className="flex flex-col text-lg font-semibold space-y-4">
             <Link to="/">You must watch</Link>
             <Link to="/">Recent release</Link>
             <Link to="/">Top IMDB</Link>
