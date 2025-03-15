7f6e5e72bc17c7845457ee21c9932e2cc1b1e019
leomontenegro2104 <eng.leonardomontenegro@gmail.com>
2025-03-15 09:54:37 -0300

    refactor: detail page

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,15 +1,11 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import apiConfig from '../../api/apiConfig';
-
 import { Category, Movie } from '../../context/TmdbContext';
 import { useTmdb } from '../../hooks/useTmdb';
-
 import CastList from '../../components/molecules/CastList/CastList';
 import VideoList from '../../components/molecules/VideoList/VideoList';
 import MovieGrid from '../../components/molecules/MovieGrid/MovieGrid';
-
-import './detail.scss';
 
 interface Params {
   category: Category;
@@ -37,17 +33,20 @@ const Detail: React.FC = () => {
       {movieDetail && (
         <>
           <div
-            className="banner"
+            className="banner relative h-[50vh] bg-center bg-cover bg-no-repeat"
             style={{
               backgroundImage: `url(${apiConfig.originalImage(
                 movieDetail.backdrop_path || movieDetail.poster_path
               )})`,
             }}
-          ></div>
-          <div className="mb-3 movie-content container">
-            <div className="movie-content__poster">
+          >
+            <div className="absolute top-0 left-0 w-full h-full bg-black/60"></div>
+            <div className="absolute bottom-0 left-0 w-full h-[100px] bg-gradient-to-t from-[#0f0f0f] to-[rgba(0,0,0,0)]"></div>
+          </div>
+          <div className="movie-content container mx-auto px-8 -mt-[200px] relative mb-12 flex flex-col md:flex-row">
+            <div className="movie-content__poster flex-1 hidden md:block">
               <div
-                className="movie-content__poster__img"
+                className="movie-content__poster__img bg-center bg-cover bg-no-repeat rounded-[30px] pb-[165%]"
                 style={{
                   backgroundImage: `url(${apiConfig.originalImage(
                     movieDetail.poster_path || movieDetail.backdrop_path
@@ -55,31 +54,36 @@ const Detail: React.FC = () => {
                 }}
               ></div>
             </div>
-            <div className="movie-content__info">
-              <h1 className="title">{movieDetail.title}</h1>
-              <div className="genres">
+            <div className="movie-content__info w-full md:w-[70%] md:pl-8 relative space-y-8">
+              <h1 className="title text-[4rem] leading-none text-white">
+                {movieDetail.title}
+              </h1>
+              <div className="genres flex space-x-2">
                 {((movieDetail as MovieWithGenres).genres)?.slice(0, 5).map((genre, i) => (
-                  <span key={i} className="genres__item">
+                  <span
+                    key={i}
+                    className="genres__item py-2 px-6 border-2 border-white rounded-[30px] text-xs font-semibold bg-[#0f0f0f] text-white"
+                  >
                     {genre.name}
                   </span>
                 ))}
               </div>
-              <p className="overview">{movieDetail.overview}</p>
+              <p className="overview text-white">{movieDetail.overview}</p>
               <div className="cast">
-                <div className="section__header">
-                  <h2>Casts</h2>
+                <div className="section__header mb-4 flex items-center justify-between">
+                  <h2 className="text-2xl text-white font-bold">Casts</h2>
                 </div>
                 <CastList id={movieDetail.id} />
               </div>
             </div>
           </div>
-          <div className="container">
-            <div className="section mb-3">
+          <div className="container mx-auto px-8">
+            <div className="section mb-12">
               <VideoList id={movieDetail.id} />
             </div>
-            <div className="section mb-3">
-              <div className="section__header mb-2">
-                <h2>Similar</h2>
+            <div className="section mb-12">
+              <div className="section__header mb-4 flex items-center justify-between">
+                <h2 className="text-2xl text-white font-bold">Similar</h2>
               </div>
               <MovieGrid category={category as Category} type="similar" id={movieDetail.id} />
             </div>
