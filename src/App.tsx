9e954c65f4c0cbe9803195a69d087b9f1f2b2870
leomontenegro2104 <eng.leonardomontenegro@gmail.com>
2025-03-15 10:34:05 -0300

    feat: login and singup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
-import { BrowserRouter } from "react-router-dom"
-import AppRoutes from "./routers/Routes"
-import { TmdbProvider } from "./context/TmdbContext"
-import Footer from "./components/molecules/Footer/Footer"
-import Header from "./components/molecules/Header/Header"
+import React from 'react';
+import { BrowserRouter, useLocation } from 'react-router-dom';
+import AppRoutes from './routers/Routes';
+import { TmdbProvider } from './context/TmdbContext';
+import Footer from './components/molecules/Footer/Footer';
+import Header from './components/molecules/Header/Header';
+
+const Layout: React.FC = () => {
+  const location = useLocation();
+  const hideHeader = ['/login', '/signup'].includes(location.pathname);
+  return (
+    <div className="flex flex-col min-h-screen min-w-screen">
+      {!hideHeader && <Header />}
+      <main className="flex-grow">
+        <AppRoutes />
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 function App() {
   return (
     <TmdbProvider>
       <BrowserRouter>
-        <div className="flex flex-col min-h-screen min-w-screen">
-          <Header />
-          <main className="flex-grow">
-            <AppRoutes />
-          </main>
-          <Footer />
-        </div>
+        <Layout />
       </BrowserRouter>
     </TmdbProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,15 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/tmovie.png';
+import Input from '../components/atoms/Input/Input';
+import Button from '../components/atoms/Button/Button';
 
-export interface LogInProps {
+const LogIn: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-}
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
 
-const LogIn: React.FC<LogInProps> = () => {
   return (
-    <div>
-      LogIn
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-900 px-4">
+      <div className="mb-8 flex flex-col items-center">
+        <img src={logo} alt="tMovies logo" className="w-16 mb-4" />
+        <h1 className="text-3xl text-white font-bold">Log In</h1>
+      </div>
+      <form onSubmit={handleSubmit} className="w-full max-w-sm bg-gray-800 p-8 rounded-lg shadow-lg">
+        <div className="mb-4">
+          <Input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="w-full"
+          />
+        </div>
+        <div className="mb-6">
+          <Input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full"
+          />
+        </div>
+        <Button className="w-full">
+          Log In
+        </Button>
+      </form>
+      <p className="mt-4 text-white">
+        Don't have an account?{' '}
+        <Link to="/signup" className="text-red-500 underline">
+          Sign Up
+        </Link>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default LogIn
+export default LogIn;
