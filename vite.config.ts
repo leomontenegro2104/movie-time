0e491aa54e5d0fe1e070d8e1cea925499281fea4
leomontenegro2104 <eng.leonardomontenegro@gmail.com>
2025-03-16 13:15:28 -0300

    fix: outdir path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vite.dev/config/
 export default defineConfig({
   server: { port: 3000 },
   plugins: [react(), tsconfigPaths()],
@@ -16,6 +15,6 @@ export default defineConfig({
   },
   build: {
     sourcemap: true,
-    outDir: 'build',
+    outDir: 'dist',
   },
 });
